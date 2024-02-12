2916e5c9868c02a05463426d2c12fffb10fb19b8
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-02-12 15:29:22 -0300

    feat: entities and repositories with seed

--- src/main/java/dev/rafaelguilherme/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/dev/rafaelguilherme/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,17 +1,22 @@
 package dev.rafaelguilherme.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.rafaelguilherme.certification_nlw.modules.students.controllers.dto.VerifyHasCertificationDTO;
+import dev.rafaelguilherme.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
+
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
     
     public boolean execute( VerifyHasCertificationDTO dto ) {
-        if (dto.getEmail().equals("rafael@email.com") && dto.getTechnology().equals("Nodejs")) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
-
         return false;
     }
 }
